1aa30baafc4f3eead582e1056d4a69b431a8d430
Li-Vincent <630927564@qq.com>
2018-12-09 01:27:42 +0800

    test for jenkins

---
+++ src/test/java/com/automation/test/BaiduTest2.java
@@ -0,0 +1,38 @@
+package com.automation.test;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.automation.BaseTest;
+
+public class BaiduTest2 {
+
+	@BeforeMethod
+	public void beforeMethod() {
+		BaseTest.openBaidu();
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		BaseTest.quit();
+	}
+
+	@Test
+	public void test() {
+        BaseTest.getDriver().findElement(By.xpath(".//*[@id='kw']")).sendKeys("selenium");
+        BaseTest.getDriver().findElement(By.xpath(".//*[@id='su']")).click();
+
+        try {
+			BaseTest.getDriver().wait(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        Assert.assertTrue(BaseTest.getDriver().getPageSource().contains("ä¯ÀÀÆ÷×Ô¶¯»¯²âÊÔ¿ò¼Ü"));
+        Reporter.log("ËÑË÷seleniumµÄ²âÊÔÓÃÀı");
+	}
+
+}
